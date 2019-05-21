771b0fcbbcd52bb66620811cc047a868c615bdc5
Andrew <>
2019-05-21 09:42:44 -0700

    first vesion, first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      ticket: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      ticket: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const paramsTicket = this.state.ticket;
+    const url = `https://revolve.atlassian.net/browse/${paramsTicket}`;
+    const win = window.open(url, '_blank');
+    win.focus();
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <h1>Enter Jira Ticket</h1>
+        <form className="App-form" onSubmit={this.handleSubmit}>
+          <input autoFocus
+                 className="App-input"
+                 type = "text"
+                 name = "ticket"
+                 value = {this.state.ticket}
+                 onChange = {this.handleChange}
+                 placeholder = "Enter: ux-7290, mar-3245, etc..." />
+          <button className="App-submit" type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+};
 
 export default App;
