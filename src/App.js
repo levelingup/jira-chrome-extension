1cc02eb43239ced67f55a9d4425f3374a4ab0fd4
Andrew Liu <ahliu1@gmail.com>
2020-02-04 23:39:27 -0800

    new search

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  prefix = () => {
+
+  }
+
   handleChange(event) {
     this.setState({
       ticket: event.target.value
@@ -20,16 +24,34 @@ class App extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+
+    const prefix = ['ac-', 'al-', 'an-', 'andro-' , 'cs-', 'data-', 'dev-', 'exal-', 'gen-', 'in-' , 'ip-', 'ipf-', 'klar-', 'mar-', 'mer-', 'op-' , 'grap-', 'qa-', 'ruzirwzin', 'tec-', 'temp-', 'tr-', 'tri-', 'trm-', 'ux-']
     const paramsTicket = this.state.ticket;
-    const url = `https://revolve.atlassian.net/browse/${paramsTicket}`;
+    //https://revolve.atlassian.net/issues/?jql=text%20~%20%22fdafasdsa%22
+    // checks to see if its 1 word (most likely jira ticket)
+
+    let url = '';
+    if(paramsTicket.split(' ').length === 1){
+      
+      // check if the word is within our jira ticket #s
+      if (prefix.some(v => paramsTicket.includes(v))) {
+        url = `https://revolve.atlassian.net/browse/${paramsTicket}`;
+      } else {
+        url = `https://revolve.atlassian.net/secure/QuickSearch.jspa?searchString=${paramsTicket}`
+      }
+
+    } else {
+      url = `https://revolve.atlassian.net/secure/QuickSearch.jspa?searchString=${paramsTicket}`
+    }
     const win = window.open(url, '_blank');
     win.focus();
+    
   }
 
   render(){
     return(
       <div className="App">
-        <h1>Enter Jira Ticket</h1>
+        <h1>Enter Jira Ticket or Search</h1>
         <form className="App-form" onSubmit={this.handleSubmit}>
           <input autoFocus
                  className="App-input"
@@ -37,9 +59,10 @@ class App extends React.Component {
                  name = "ticket"
                  value = {this.state.ticket}
                  onChange = {this.handleChange}
-                 placeholder = "Enter: ux-7290, mar-3245, etc..." />
+                 placeholder = "Enter: ux-7290, mar-3245, shopping bag recs" />
           <button className="App-submit" type="submit">Submit</button>
         </form>
+        <a href="mailto:andrew.liu@revolve.com?subject=Google Chrome Extension Bug Report" target="_blank" rel="noopener noreferrer">report a bug</a>
       </div>
     );
   }
